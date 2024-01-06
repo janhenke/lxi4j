968b44df00fe8f2bbadef2854399e1dfd339b527
Jan Henke <Jan.Henke@taujhe.de>
2024-01-06 19:38:12 +0100

    Add placeholder test

---
+++ core/src/test/java/de/taujhe/lxi4j/PlaceholderTest.java
@@ -0,0 +1,18 @@
+package de.taujhe.lxi4j;
+
+import org.junit.jupiter.api.Test;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+/**
+ * Placeholder unit test to verify JUnit is set up correctly.
+ */
+public class PlaceholderTest
+{
+	@Test
+	void dummy()
+	{
+		final String string = "";
+		assertThat(string).isNotNull();
+	}
+}
